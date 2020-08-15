471c75a5573b77e608c4073132466c88a9778041
aniket8892 <aniket@sp-assurance.com>
2020-08-15 21:04:10 +0530

    updates

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zi7&pf=qv5n^0ar#ehj08qtse+m)4_cw=ut%shhl0p$51x58r8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default = 0))
 
 ALLOWED_HOSTS = ['*']
 
